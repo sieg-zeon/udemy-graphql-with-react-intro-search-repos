1bab9277a7d05ce127f101029d792c06a316ad45
jion-kozono <zeon66190329@gmail.com>
2020-06-21 18:43:06 +0900

    apply query and variables

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react'
 import { ApolloProvider } from 'react-apollo'
 import { Query } from 'react-apollo'
 import client from './client'
-import { ME } from './graphql'
+import { SEARCH_REPOSITORIES } from './graphql'
 
+const VARIBLES = {
+  after: null,
+  before: null,
+  first: 5,
+  last: null,
+  query: "フロントエンドエンジニア"
+}
 class App extends Component {
-  render(){
+  constructor(props) {
+    super(props)
+    this.state = VARIBLES
+  }
+  render() {
+    const { query, first, last, after, before} =this.state
     return (
       <ApolloProvider client={client}>
-        <div>Hello graphql</div>
-
-        <Query query={ME}>
+        <Query
+          query={SEARCH_REPOSITORIES}
+        variables={{query, first, last, after, before}}
+        >
           {
             ({ loading, error, data }) => {
               if (loading) return 'loading...'
               if (error) return `Error! ${error.message}`
-
-              return <div>{data.user.name}</div>
+              console.log({data})
+              return <div></div>
             }
           }
         </Query>
--- src/graphql.js
+++ src/graphql.js
@@ -1,10 +1,28 @@
 import gql from 'graphql-tag'
-
-export const ME = gql`
-    query me {
-        user(login: "jion-kozono"){
-        name
-        avatarUrl
+export const SEARCH_REPOSITORIES = gql`
+query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+    search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY) {
+        repositoryCount
+        pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+        }
+        edges {
+            cursor
+            node {
+                ...on Repository {
+                    id
+                    name
+                    url
+                    stargazers {
+                        totalCount
+                    }
+                    viewerHasStarred
+                }
+            }
         }
     }
+}
 `
